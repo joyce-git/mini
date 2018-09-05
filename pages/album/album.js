4d39df67be65ff2c0cfc679b46b97ad78c7e7b8b
Joyce Zhang <joyce.zhang@intersystems.com>
2018-09-05 16:50:12 -0400

    update albumm page

---
+++ pages/album/album.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    server: 'https://safe-savannah-64671.herokuapp.com/links/',
+    images: [],
+    indicatorDots: true,
+    circular: true,
+  },
+
+  onLoad: function (e) {
+    let that = this;
+    let link = '5b90397de1399f6c8d4d4bdf';
+    if (e.scene) {
+      link = decodeURIComponent(e.scene)
+    };
+    let url = that.data.server + link;
+    wx.request({
+      url: url,
+      success: function(res) {
+        that.setData({images: res.data.images});
+      }
+    })
+  }
+})
